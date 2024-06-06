86ed10363298cd22e8f45df97fc52a509e66097e
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-06-06 17:46:25 +0300

    Implement UDP traceroute

--- src/traceroute.py
+++ src/traceroute.py
@@ -1,60 +1,128 @@
 import socket
-import traceback
+import os
+import sys
+import signal
+from ipaddress import IPv4Address
 
-# socket de UDP
-udp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+import requests
 
-# socket RAW de citire a răspunsurilor ICMP
-icmp_recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-# setam timout in cazul in care socketul ICMP la apelul recvfrom nu primeste nimic in buffer
-icmp_recv_socket.settimeout(3)
+USER_AGENT = "UDP Traceroute Homework for Networks course at University of Bucharest (https://networks.hypha.ro/)"
+USER_AGENT_B = USER_AGENT.encode("ascii")
 
-def traceroute(ip, port):
-    # setam TTL in headerul de IP pentru socketul de UDP
-    TTL = 64
-    udp_send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, TTL)
+# cSpell: ignoreRegExp socket.\w+
 
-    # trimite un mesaj UDP catre un tuplu (IP, port)
-    udp_send_sock.sendto(b'salut', (ip, port))
+def get_ip_info(ip: IPv4Address, timeout: int) -> str:
+    if ip.is_private:
+        return "No IP info available: private range"
 
-    # asteapta un mesaj ICMP de tipul ICMP TTL exceeded messages
-    # in cazul nostru nu verificăm tipul de mesaj ICMP
-    # puteti verifica daca primul byte are valoarea Type == 11
-    # https://tools.ietf.org/html/rfc792#page-5
-    # https://en.wikipedia.org/wiki/Internet_Control_Message_Protocol#Header
-    addr = 'done!'
+    header = {"user-agent": USER_AGENT}
+    query_url = f"http://ip-api.com/json/{ip}?fields=status,message,country,regionName,city,isp,org,as"
     try:
-        data, addr = icmp_recv_socket.recvfrom(63535)
-    except Exception as e:
-        print("Socket timeout ", str(e))
-        print(traceback.format_exc())
-    print (addr)
-    return addr
+        response = requests.get(query_url, headers=header, timeout=timeout)
+    except requests.exceptions.RequestException as e:
+        return f"No IP info available: Got exception {e.__class__.__name__} when sending request to API, exception message: {e}"
 
-'''
- Exercitiu hackney carriage (optional)!
-    e posibil ca ipinfo sa raspunda cu status code 429 Too Many Requests
-    cititi despre campul X-Forwarded-For din antetul HTTP
-        https://www.nginx.com/resources/wiki/start/topics/examples/forwarded/
-    si setati-l o valoare in asa fel incat
-    sa puteti trece peste sistemul care limiteaza numarul de cereri/zi
+    if response.status_code != 200:
+        print(f"Non-200 status code {response.status_code} from query at {query_url}", file=sys.stderr)
+        return f"No IP info available: Got status code {response.status_code} from API"
 
-    Alternativ, puteti folosi ip-api (documentatie: https://ip-api.com/docs/api:json).
-    Acesta permite trimiterea a 45 de query-uri de geolocare pe minut.
-'''
+    try:
+        ip_info = response.json()
+    except requests.exceptions.JSONDecodeError:
+        return "No IP info available: Could not decode JSON response from API"
 
-# exemplu de request la IP info pentru a
-# obtine informatii despre localizarea unui IP
-fake_HTTP_header = {
-                    'referer': 'https://ipinfo.io/',
-                    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
-                   }
-# informatiile despre ip-ul 193.226.51.6 pe ipinfo.io
-# https://ipinfo.io/193.226.51.6 e echivalent cu
-raspuns = requests.get('https://ipinfo.io/widget/193.226.51.6', headers=fake_HTTP_header)
-print (raspuns.json())
+    assert isinstance(ip_info, dict)
+    if ip_info["status"] != "success":
+        return f"No IP info available: {ip_info['message']}"
 
-# pentru un IP rezervat retelei locale da bogon=True
-raspuns = requests.get('https://ipinfo.io/widget/10.0.0.1', headers=fake_HTTP_header)
-print (raspuns.json())
+    def get(key: str, name: str) -> str:
+        value = ip_info.get(key, None)
+        if value is None:
+            return f"Unknown {name}"
+        return f"{name}: \"{value}\""
+    country = get("country", "Country")
+    region = get("regionName", "Region")
+    city = get("city", "City")
+    isp = get("isp", "ISP")
+    org = get("org", "Organization")
+    as_ = get("as", "Autonomous System")
 
+    return f"{country}, {region}, {city}, {isp}, {org}, {as_}"
+
+class UDPSendError(Exception):
+    pass
+
+def traceroute(ip: IPv4Address, ttl: int, udp_send_sock: socket.socket, icmp_recv_socket: socket.socket) -> IPv4Address | None:
+    udp_send_sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+
+    destination_port = 65535
+
+    content = USER_AGENT_B
+    bytes_sent = udp_send_sock.sendto(content, (str(ip), destination_port))
+    if bytes_sent != len(content):
+        raise UDPSendError(f"Could not send all {len(content)} bytes to {ip}:{destination_port}")
+    try:
+        # TODO: check _data to see if the response is TTL exceeded, destination unreachable, or port unreachable
+        # TODO: TTL exceeded includes source, destination, length, checksum, check if it's from this traceroute
+        # TODO: some routers send more than 8 bytes even though only 8 are required, if so check that as well
+        _data, (addr, _port) = icmp_recv_socket.recvfrom(63535)
+        return IPv4Address(addr)
+    except TimeoutError as _e:
+        return None
+
+def main():
+    signal.signal(signal.SIGINT, lambda _signal, _frame: sys.exit(1)) # exit on Ctrl+C without exception trace
+
+    timeout = 5
+    consecutive_timeout_limit = 10
+
+    trace_destination = sys.argv[1].strip()
+    try:
+        # is it an IP address?
+        trace_ip = IPv4Address(trace_destination)
+    except ValueError:
+        # try resolving it with DNS
+        try:
+            trace_ip = IPv4Address(socket.gethostbyname(trace_destination))
+        except socket.gaierror as e:
+            print(f"Could not resolve IPv4 address for {trace_destination}: {e}")
+            sys.exit(1)
+
+    udp_send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+    udp_send_sock.bind(("", 0)) # let the OS choose a source port
+
+    icmp_recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    icmp_recv_socket.settimeout(timeout)
+
+    if os.name == "nt":
+        # Required for ICMP socket on Windows
+        # TODO: this is not enough to work on Windows, WireShark shows the TTL exceeded packets arrive but the socket does not receive them
+        print("WARNING: traceroute.py appears not to receive ICMP TTL exceeded packets on Windows, likely won't work")
+        host = socket.gethostbyname(socket.gethostname())
+        icmp_recv_socket.bind((host, 0))
+        icmp_recv_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        icmp_recv_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    timeouts = 0
+    for ttl in range(1, sys.maxsize):
+        hop_ip = traceroute(trace_ip, ttl, udp_send_sock, icmp_recv_socket)
+        if hop_ip is None:
+            ip_info = f"No response within {timeout} seconds"
+            timeouts += 1
+        else:
+            ip_info = get_ip_info(hop_ip, timeout)
+            timeouts = 0
+        placeholder = "?.?.?.?"
+        print(f"{ttl}. {hop_ip or placeholder} - {ip_info}")
+        if hop_ip == trace_ip:
+            print("Reached destination")
+            break
+        if timeouts >= consecutive_timeout_limit:
+            print(f"Limit of {consecutive_timeout_limit} consecutive timeouts reached")
+            break
+
+    icmp_recv_socket.close()
+    udp_send_sock.close()
+
+if __name__ == "__main__":
+    main()

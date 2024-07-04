1a83c9c9eceed51edc0f4f15ea8447f5c2bd0ec6
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-07-04 16:26:07 +0300

    Fix zero division error in speed test

--- src/speed_test_serv.py
+++ src/speed_test_serv.py
@@ -14,7 +14,11 @@ def main():
             received_from_tunnel = sock.pop_data(999999999, 0)
             bytes_received += len(received_from_tunnel)
             elapsed_time = time.time() - start_time
-            print(f"Received {bytes_received} bytes in {elapsed_time:.6f} seconds - average speed: {bytes_received/elapsed_time:.6f} bytes/s", end="\r")
+            try:
+                avg_speed = bytes_received/elapsed_time
+            except ZeroDivisionError:
+                avg_speed = float('nan')
+            print(f"Received {bytes_received} bytes in {elapsed_time:.6f} seconds - average speed: {avg_speed:.6f} bytes/s", end="\r")
             if len(received_from_tunnel) != 0:
                 sock.push_data(bytes_received.to_bytes(8, "big"))
     except KeyboardInterrupt:
